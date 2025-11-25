c7d211a46ed85e7c9f4589718b46dd4ac6a9e8b0
arcticmath <arcticmath@gmail.com>
2025-11-25 15:03:50 -0600

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -7,9 +7,4 @@ self.__uv$config = {
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
-    bare: {
-        version: 2,
-        path: '/seal/'
-    },
-    wisp: '/wisp/'
 };
