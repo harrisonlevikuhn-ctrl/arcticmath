e5b9e87822722c5887a09bd041693b5b2644df27
arcticmath <arcticmath@gmail.com>
2025-12-08 11:11:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ server.on("upgrade", (req, socket, head) => {
 });
 
 server.on('listening', () => {
-  console.log(chalk.bgBlue.white.bold`  Welcome to Doge V4, user!  ` + '\n');
+  console.log(chalk.bgBlue.white.bold`  Welcome to Arctic 1.0, user!  ` + '\n');
   console.log(chalk.cyan('-----------------------------------------------'));
   console.log(chalk.green('  🌟 Status: ') + chalk.bold('Active'));
   console.log(chalk.green('  🌍 Port: ') + chalk.bold(chalk.yellow(server.address().port)));
